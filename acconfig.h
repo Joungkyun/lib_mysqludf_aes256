e45966a3149ed54936f88a1a1fb352b5190a9b34
JoungKyun Kim <joungkyun@daum.net>
2013-12-23 08:38:43 +0000

    add mysql_aes256_conflict.h

---
+++ acconfig.h
@@ -0,0 +1,13 @@
+/* $Id: $ */
+#ifndef LIB_MYSQLUDF_AES256_CONFIG_H
+#define LIB_MYSQLUDF_AES256_CONFIG_H
+
+/* config.h:  a general config file */
+
+#include "mysql_aes256_conflict.h"
+
+@TOP@
+
+@BOTTOM@
+
+#endif /*  LIB_MYSQLUDF_AES256_CONFIG_H */
